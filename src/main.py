3c38f050901113468a780cd4340f9a44ed681f66
ahan-nz <14235@coderacademy.edu.au>
2023-05-13 02:42:32 +1000

    finished main code

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from datetime import date
 import sys
 import contactrx
 
+print('\nWelcome to Alicia\'s contact lens ordering programme, made for optometrists and clinic staff.\n')
+
 # Get contact lens prescription and assign to variable
 try:
     clrx = contactrx.clrx()
@@ -29,9 +31,14 @@ for index, item in enumerate(mod):
     print(f"{index+1}. {item}")
 
 # While loop until user enters a valid selection
-modality = ''
-while modality.lower() not in mod:
-    modality = input('Your choice: ')
+while True:
+        modality = input('Your choice: ')
+
+        if modality in mod:
+            break
+        
+        else:
+            print('Invalid selection, please choose from \'daily\', \'fortnightly\', or \'monthly\'.')
 
 # Opening selection of contact lenses from external txt file, depending on the modality
 print('Please choose from the following lenses: ')
@@ -53,60 +60,72 @@ for line in f:
 f. close()
 
 # User input chosen lens, patient ID and number of lenses needed
-lens = input('Enter your choice: ')
+lens = input('Enter your choice, or specify another lens: ')
 id = input('Enter patient ID: ')
 
-try:
-    amount = int(input('Enter pairs of lenses needed: '))
-except ValueError:
-    print('Please enter an integer value.')
+while True:
+    try:
+        amount = int(input('Enter pairs of lenses needed: '))
+
+        if amount >= 1:
+            break
+            
+        else:
+            print('Amount can\'t be less than 1!')
+
+    except ValueError:
+        print('Value entered wasn\'t an integer.')
+        continue
+
+    except Exception:
+        print('Oops! Something went wrong. Please try again.')
+        sys.exit(1)
+
+for retry in range(5):
+    # Confirm with user that the details are correct
+    confirm = input(f"Your order: {amount} pair(s) of {clrx:.2f}DS {lens} lenses, for patient #{id}. Y/N: ")
+
+    if confirm.lower() == 'y':
+        today = date.today().strftime('%d-%m-%Y')
+        order = {'Date': today, 'Patient ID': id, 'Modality': modality, 'Lens': lens, 'CL Rx': clrx, 'Amount (pairs)': amount}
+        fields = ['Date', 'Patient ID', 'Modality', 'Lens', 'CL Rx', 'Amount (pairs)']
+
+        for retry in range(5):
+            # Has this order been placed
+            if_ordered = input('Has this order already been placed? Y/N: ')
+
+            # If yes, print to ordered.csv file with today's date
+            if if_ordered.lower() == 'y':
+                with open('ordered.csv', 'a') as ordered:
+                    writer = csv.DictWriter(ordered, fieldnames = fields)
+                    writer.writerow(order)
+                break
+
+            # else, print to pending.csv file with today's date
+            elif if_ordered.lower() == 'n':
+                with open('pending.csv', 'a') as ordered:
+                    writer = csv.DictWriter(ordered, fieldnames = fields)
+                    writer.writerow(order)
+                break
+                    
+            else:
+                print('Please enter \'Y\' or \'N\'.')
+
+        else:
+            print('Too many invalid choices, programme ending.')
+            sys.exit(1)
+
+        break
+
+    elif confirm.lower() == 'n':
+        print('Please start again.')
+        sys.exit(1)
+
+    else:
+        print('Please enter \'Y\' or \'N\'.')
+
+else:
+    print('Too many invalid choices, programme ending.')
     sys.exit(1)
 
-# Confirm with user that the details are correct
-confirm = input(f"Your order: {amount} pair(s) of {clrx:.2f}DS {lens} lenses, for patient #{id}. Y/N: ")
-
-if confirm.lower() == 'y':
-    today = date.today().strftime('%d-%m-%Y')
-    order = {'Date': today, 'Patient ID': id, 'Lens': lens, 'CL Rx': clrx, 'Amount (pairs)': amount}
-    fields = ['Date', 'Patient ID', 'Lens', 'CL Rx', 'Amount (pairs)']
-
-    # Has this order been placed
-    if_ordered = input('Has this order already been placed? Y/N: ')
-
-    # If yes, print to ordered.csv file with today's date
-    if if_ordered.lower() == 'y':
-        with open('ordered.csv', 'a') as ordered:
-            writer = csv.DictWriter(ordered, fieldnames = fields)
-            writer.writerow(order)
-
-    # else, print to pending.csv file with today's date
-    elif if_ordered.lower() == 'n':
-        with open('pending.csv', 'a') as ordered:
-            writer = csv.DictWriter(ordered, fieldnames = fields)
-            writer.writerow(order)\
-            
-    else:
-        print('Please enter \'Y\' or \'N\'')
-        pass # loop back to line 74
-
-elif confirm.lower() == 'n':
-    print('Please start again.')
-    pass #loop back to line 1
-
-else:
-    print('Please enter \'Y\' or \'N\'')
-    pass # loop back to line 66
-
-# Would you like to add another order? 
-moreorders = input('Would you like to add another order? Y/N: ')
-
-if moreorders.lower() == 'y':
-    pass # loop back to line 1
-
-# If no, end programme
-elif moreorders.lower() == 'n':
-    print('Thank you for using our programme.')
-
-else:
-    print('Please enter \'Y\' or \'N\'')
-    pass # Loop back to line 101
+print('Thank you for using our programme.')
